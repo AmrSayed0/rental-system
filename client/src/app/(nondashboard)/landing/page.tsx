4ae73219e3c8799a70212f3b939562474c7a94c2
AmrSayed0 <amracademycode@gmail.com>
2025-03-06 23:54:47 +0200

    Add landing page sections: Features, Discover, and CallToAction

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,9 +1,15 @@
+import CallToActionSection from "./CallToActionSection";
+import DiscoverSection from "./DiscoverSection";
+import FeaturesSection from "./FeaturesSection";
 import HeroSection from "./HeroSection";
 
 const Landing = () => {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
     </div>
   );
 };
